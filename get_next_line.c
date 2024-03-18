c103e7dcc76a62ad51ad4c3846ee25cc4153784f
mmaksimo <mmaksimo@ubuntuhome.myguest.virtualbox.org>
2024-03-18 22:13:12 +0700

    hi new ver

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: mmaksimo <mmaksimo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/09 20:32:21 by mmaksimo          #+#    #+#             */
-/*   Updated: 2024/03/13 20:23:15 by mmaksimo         ###   ########.fr       */
+/*   Updated: 2024/03/18 22:16:41 by mmaksimo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,74 +18,38 @@
 
 char	*get_next_line(int fd)
 {
-	static char		buffer[BUFFER_SIZE];
-	static size_t	i;
-	size_t			line_len;
-	int				read_ret;
-	char			*start = NULL;
-	char			*line;
-
-	line = NULL;
-
-	if (fd < 0)// || BUFFER_SIZE > 65536) //Can it be more?
-		return (NULL);
-
-	line_len = 0;
-	read_ret = 1;
-
-	while (line_len < BUFFER_SIZE)
+	char		buffer[BUFFER_SIZE + 1]; // +1 for NUL terminator
+	ssize_t		read_ret;
+	
+	while (read_ret)
 	{
-		read_ret = read(fd, &buffer[i], sizeof(char));
-
-		if (read_ret <= 0)
-		{
-			i = 0;
-			ft_memset(buffer, '\0', sizeof(buffer));
-			if (line)
-				free(line);
-			return (NULL);
-		}
-		if (buffer[i] == '\n' || buffer[i] == '\0')
-		{
-			if (buffer[i] == '\n')
-			{
-				i++;
-				line_len++;
-			}
-			start = &buffer[i - line_len];
-			line = (char *) malloc((line_len + 1) * sizeof(char));
-			if (line == NULL)
-			{
-				free(line);
-				return (NULL);
-			}
-			ft_strlcpy(line, start, line_len + 1);
-			ft_memset(buffer, '\0', sizeof(buffer));
-			break ;
-		}
-		line_len++;
-		i++;
-		if (i >= BUFFER_SIZE || line_len > BUFFER_SIZE) // Added check to prevent buffer overflow
-		{
-			i = 0;
-			ft_memset(buffer, '\0', sizeof(buffer));
-			if (line)
-				free(line);
-			return (NULL);
-		}
+		read_ret = read(fd, buffer, BUFFER_SIZE);
+		if (read_ret == -1)
+			break ; // check for 0 here?
+			/*
+			- Make sure to handle the case 
+			where read_ret might be a positive value 
+			greater than zero but less than BUFFER_SIZE. 
+			This indicates a partial read, which might require 
+			further processing depending on your use case.
+			*/
+		
+		
 	}
-	if (line_len == BUFFER_SIZE)
-	{
-		start = &buffer[i - line_len];
-		line = (char *) malloc((line_len + 1) * sizeof(char));
-		if (line == NULL)
-		{
-			free(line);
-			return (NULL);
-		}
-		ft_strlcpy(line, start, line_len + 1);
-	}
-	return (line);
+	return (NULL);
+	
+	/*
+	1) allocate buffer[BUFFER_SIZE]
+	2) read BUFFER_SIZE chars from fd to buffer 
+		- read(fd, buffer, BUFFER_SIZE)
+		or 
+		- read(fd, &buffer[i], sizeof(char))
+		
+	3) ft_strjoin read buffer to char *line (static?)
+	4) check if '\n' is in buffer (before or after joining?)
+	5) do until EOF
+	6) on read = 0 -- return NULL;
+	*/
 }
 
 /*
--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: mmaksimo <mmaksimo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/09 21:05:26 by mmaksimo          #+#    #+#             */
-/*   Updated: 2024/03/13 18:54:22 by mmaksimo         ###   ########.fr       */
+/*   Updated: 2024/03/18 22:14:12 by mmaksimo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -35,20 +35,16 @@ int main(void)//int argc, char *argv[])
 		return (-1);
 	}
 	char *line1;
-	// int fd = open("/home/mmaksimo/francinette/tests/get_next_line/gnlTester/files/41_no_nl", O_RDWR);
-
-	// char *line1 = get_next_line(fd);
-
-	// if (!strcmp(line1,"01234567890123456789012345678901234567890"))
-	// 	printf("strings diff!\n");
-
+	// line1 = get_next_line(fd);
+	// printf("%s", line1);
+	// free(line1);
 	int i = 0;
 	while (i < 30)
 	{
 		line1 = get_next_line(fd);
 		if (line1 == NULL)
-		{
-			printf("NULL or EOF\n");
+		{	
+			printf("Aborted function or EOF\n");	
 			break ;
 		}
 		printf("%s", line1);
